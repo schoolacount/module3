ab12977d8ff3e0b1ca3b5babbfe59f97c06434bb
schoolacount <81690599+schoolacount@users.noreply.github.com>
2021-03-31 22:20:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+let data;
+
     (async() => {
 let response = await fetch('https://schoolacount.github.io/module3/data.json');
-let data = await response.json(); 
+data = await response.json(); 
 console.log(data)
 })();
+
