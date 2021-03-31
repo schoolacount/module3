16ab10625c9160d5092bc606d0251528c5595f19
schoolacount <81690599+schoolacount@users.noreply.github.com>
2021-03-31 22:04:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-    fetch('https://schoolacount.github.io/module3/data.json').then(response => response.json()).then(data => console.log(data));
+const data = fetch('https://schoolacount.github.io/module3/data.json').then(response => response.json()).then(data => data);
+console.log(data);
